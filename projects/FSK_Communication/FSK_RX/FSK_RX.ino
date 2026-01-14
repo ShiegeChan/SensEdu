bd3992d669292078170ccba50a1986e29ca0050a
Ferni41359 <Fernando.GomezGarcia@infineon.com>
2026-01-14 13:51:26 +0100

    feat: FSK demodulation working

--- projects/FSK_Communication/FSK_RX/FSK_RX.ino
+++ projects/FSK_Communication/FSK_RX/FSK_RX.ino
@@ -3,22 +3,22 @@
 /* errors */
 static uint32_t lib_error = 0;
 uint8_t error_led = D86;
-
+const int offset = 5; //Wait for the signal to arrive, change if needed
 const int SYNC_PIN = 2;
 
-// Configure the ADC 
-const uint16_t mic_data_size = 2048*4;
+// Configure the ADC
+const uint16_t mic_data_size = 64 * 8 + offset;  //match MAX_LUT_SIZE
 SENSEDU_ADC_BUFFER(mic_data, mic_data_size);
 
 ADC_TypeDef* adc = ADC1;
 const uint8_t mic_num = 1;
-uint8_t mic_pins[mic_num] = {A1};
+uint8_t mic_pins[mic_num] = { A1 };
 SensEdu_ADC_Settings adc_settings = {
     .adc = adc,
     .pins = mic_pins,
     .pin_num = mic_num,
     .conv_mode = SENSEDU_ADC_MODE_CONT_TIM_TRIGGERED,
-    .sampling_freq = 32000*64,
+    .sampling_freq = 32800 * 32,
     .dma_mode = SENSEDU_ADC_DMA_CONNECT,
     .mem_address = (uint16_t*)mic_data,
     .mem_size = mic_data_size
@@ -32,36 +32,65 @@ void setup() {
     pinMode(error_led, OUTPUT);
     digitalWrite(error_led, HIGH);
 
+    pinMode(SYNC_PIN, INPUT);
+
     //Initializing DAC
     SensEdu_ADC_Init(&adc_settings);
     SensEdu_ADC_Enable(adc);
 }
 
-void loop () {
+void loop() {
     // Measurement is initiated by the signal from computing device
     static char serial_buf = 0;
-    
-    if(digitalRead(SYNC_PIN) == HIGH) {  // Wait for sync
-        while (1) {
-            while (Serial.available() == 0); // Wait for a signal
-            serial_buf = Serial.read();
 
-            if (serial_buf == 't') {
-                // expected 't' symbol (trigger)
-                break;
-            }
+    while (digitalRead(SYNC_PIN) == LOW) {};  // Wait for sync
+    SensEdu_ADC_Start(adc);
+    // wait for the data and send it
+    while (!SensEdu_ADC_GetTransferStatus(adc));
+    SensEdu_ADC_ClearTransferStatus(adc);
+    while (digitalRead(SYNC_PIN) == HIGH) {};  // Wait for the signal to stop
+
+    // //Print the values 
+    // for (int i = 0; i < mic_data_size; i++) {
+    //     Serial.println(mic_data[i]);
+    // }
+
+    // Decode byte
+    uint8_t received_byte = 0;
+    int samples_per_bit = 64;  // 560 / 8 = 1024
+
+    float fs = 32800 * 32;  // Your sample rate
+    float f0 = fs/samples_per_bit;       // YOUR low frequency
+    float f1 = fs*2/samples_per_bit;       // YOUR high frequency
+
+    for (int bit_pos = 7; bit_pos >= 0; bit_pos--) {
+        // Get samples for this bit
+        uint16_t* bit_samples = &mic_data[offset + (7-bit_pos) * samples_per_bit];  //maybe add an offset to skip some data int offset = 10 (?)
+
+        // Run Goertzel
+        float power_f0 = goertzel(bit_samples, samples_per_bit, f0, fs);
+        float power_f1 = goertzel(bit_samples, samples_per_bit, f1, fs);
+
+        Serial.print("Bit ");
+        Serial.print(bit_pos);
+        Serial.print(": P0=");
+        Serial.print(power_f0, 0);
+        Serial.print(" P1=");
+        Serial.println(power_f1, 0);
+
+        // Decode bit
+        bool bit = (power_f1 > power_f0 * 1.3);
+        if (bit) {
+            received_byte |= (1 << bit_pos);
         }
+  }
 
-        SensEdu_ADC_Start(adc);
-        // wait for the data and send it
-        while(!SensEdu_ADC_GetTransferStatus(adc));
-        SensEdu_ADC_ClearTransferStatus(adc);
-        serial_send_array((const uint8_t *) &mic_data, mic_data_size << 1);
-    }
-
-    
-
-    check_errors();
+    // Send decoded character
+    Serial.println();
+    Serial.print("Received text: ");
+    Serial.write(received_byte);
+    Serial.println();
+    //Serial.println(received_byte);  // Newline for readability
 }
 
 // Checking errors of the library
@@ -73,10 +102,28 @@ void check_errors() {
     }
 }
 
-// send serial data in 32 byte chunks
-void serial_send_array(const uint8_t* data, size_t size) {
-    const size_t chunk_size = 32;
-	for (uint32_t i = 0; i < size/chunk_size; i++) {
-		Serial.write(data + chunk_size * i, chunk_size);
-	}
+// Goertzel function
+float goertzel(uint16_t* samples, int N, float targetFreq, float sampleRate) {
+    float k = round((N * targetFreq) / sampleRate);  
+    float omega = (2.0 * PI * k) / N;
+    float coeff = 2.0 * cos(omega);
+
+    //DC offset of our samples
+    float sum = 0;
+    for(int i = 0; i < N; i++) {
+        sum += samples[i];
+    }
+    float dc_center = sum / N;
+
+    float q0 = 0, q1 = 0, q2 = 0;
+
+    for (int i = 0; i < N; i++) {
+        // Remove DC offset
+        float sample = (float)(samples[i] -dc_center); 
+        q0 = coeff * q1 - q2 + sample;
+        q2 = q1;
+        q1 = q0;
+    }
+    float result =  q1 * q1 + q2 * q2 - q1 * q2 * coeff;
+    return result;
 }
--- projects/FSK_Communication/FSK_TX/FSK_TX.ino
+++ projects/FSK_Communication/FSK_TX/FSK_TX.ino
@@ -1,24 +1,32 @@
 #include "SensEdu.h"
+
 // We need to create a LUT for the sine wave we want to transmit
-const uint16_t sine_lut_size_0 = 34; // sine wave size
+const uint16_t sine_lut_size_0 = 64; // sine wave size
 static uint16_t array_bit0[sine_lut_size_0] = {
-    0x000, 0x025, 0x093, 0x145, 0x236, 0x35C, 0x4AD, 0x61D, 
-    0x79E, 0x923, 0xA9D, 0xBFF, 0xD3C, 0xE49, 0xF1B, 0xFAC, 
-    0xFF6, 0xFF6, 0xFAC, 0xF1B, 0xE49, 0xD3C, 0xBFF, 0xA9D, 
-    0x923, 0x79E, 0x61D, 0x4AD, 0x35C, 0x236, 0x145, 0x093, 
-    0x025, 0x000
+0x000, 0x00A, 0x029, 0x05B, 0x0A1, 0x0F9, 0x164, 0x1DF, 
+0x26A, 0x303, 0x3A9, 0x459, 0x513, 0x5D5, 0x69C, 0x766, 
+0x833, 0x8FE, 0x9C7, 0xA8C, 0xB4A, 0xBFF, 0xCAB, 0xD4A, 
+0xDDC, 0xE60, 0xED3, 0xF34, 0xF84, 0xFC0, 0xFE8, 0xFFC, 
+0xFFC, 0xFE8, 0xFC0, 0xF84, 0xF34, 0xED3, 0xE60, 0xDDC, 
+0xD4A, 0xCAB, 0xBFF, 0xB4A, 0xA8C, 0x9C7, 0x8FE, 0x833, 
+0x766, 0x69C, 0x5D5, 0x513, 0x459, 0x3A9, 0x303, 0x26A, 
+0x1DF, 0x164, 0x0F9, 0x0A1, 0x05B, 0x029, 0x00A, 0x000
 };
 
-const uint16_t sine_lut_size_1 = 29; // sine wave size
+const uint16_t sine_lut_size_1 = 64; // sine wave size
 static uint16_t array_bit1[sine_lut_size_1] = {
-    0x000, 0x033, 0x0CB, 0x1BF, 0x303, 0x487, 0x638, 0x800, 
-    0x9C7, 0xB78, 0xCFC, 0xE40, 0xF34, 0xFCC, 0xFFF, 0xFCC, 
-    0xF34, 0xE40, 0xCFC, 0xB78, 0x9C7, 0x7FF, 0x638, 0x487, 
-    0x303, 0x1BF, 0x0CB, 0x033, 0x000
+0x000, 0x029, 0x0A1, 0x164, 0x26A, 0x3A9, 0x513, 0x69C, 
+0x833, 0x9C7, 0xB4A, 0xCAB, 0xDDC, 0xED3, 0xF84, 0xFE8, 
+0xFFC, 0xFC0, 0xF34, 0xE60, 0xD4A, 0xBFF, 0xA8C, 0x8FE, 
+0x766, 0x5D5, 0x459, 0x303, 0x1DF, 0x0F9, 0x05B, 0x00A, 
+0x00A, 0x05B, 0x0F9, 0x1DF, 0x303, 0x459, 0x5D5, 0x766, 
+0x8FE, 0xA8C, 0xBFF, 0xD4A, 0xE60, 0xF34, 0xFC0, 0xFFC, 
+0xFE8, 0xF84, 0xED3, 0xDDC, 0xCAB, 0xB4A, 0x9C7, 0x833, 
+0x69C, 0x513, 0x3A9, 0x26A, 0x164, 0x0A1, 0x029, 0x000
 };
 
 // Big buffer for the entire LUT
-const uint16_t MAX_LUT_SIZE = 8*34; // All bits ''1'' -> 1088 values
+const uint16_t MAX_LUT_SIZE = 8*64; // All bits ''0'' 
 static SENSEDU_DAC_BUFFER(lut , MAX_LUT_SIZE);
 uint8_t serial_buf;
 
@@ -29,8 +37,8 @@ const int SYNC_PIN = 2;
 
 
 // Configure the DAC1
-#define DAC_SINE_FREQ    	16000 // too low, 28-30 should be ok
-#define DAC_SAMPLE_RATE     DAC_SINE_FREQ * 64   // 64 samples per one sine cycle 
+#define DAC_SINE_FREQ    	32800 
+#define DAC_SAMPLE_RATE     DAC_SINE_FREQ * 32   // samples per one sine cycle 
 
 SensEdu_DAC_Settings dac_settings_1 = {
     .dac_channel = DAC_CH1, 
@@ -59,16 +67,14 @@ void loop () {
     //while (Serial.available() == 0); 
     // Lee el primer byte
     //serial_buf = Serial.read(); 
-    delay(100);
+    delay(5000);
     // Opcion 2: fixed bit
     serial_buf = 'U'; // U: 01010101
 
     digitalWrite(SYNC_PIN, HIGH);
-    delay(100);  // Give RX time to detect rising edge
     sendByte(serial_buf);    // Send while HIGH
     digitalWrite(SYNC_PIN, LOW);
 
-    delay(100);
     check_errors();
 }
 
@@ -82,26 +88,28 @@ void check_errors() {
 }
 
 void buildByteLUT(uint8_t data) {
-
     uint16_t position = 0;
+    const uint16_t SAMPLES_PER_BIT = 64; 
 
-    // Clear the entire LUT first (fill with DC level, e.g., 0x8000)
+    // Clear the entire LUT first (fill with DC level, e.g., 0x800)
     for (size_t i = 0; i < MAX_LUT_SIZE; i++) {
-        lut[i] = 0x0000;  // Mid-level (silence)
+        lut[i] = 0x800;  // Mid-level (silence)
     }
 
     // Build the waveform
-    // extremely weird dont do it ike this please, no negative indicies
     for (int bit_pos = 7; bit_pos >= 0; bit_pos--) {
-        bool bit = (data >> bit_pos) & 1; // do you really need this & 1
+        bool bit = (data >> bit_pos) & 1; 
         
+        // Guardamos el inicio de este bloque de bit
+        uint16_t bit_start_index = (7 - bit_pos) * SAMPLES_PER_BIT;
+
         if (bit) {
             for (int i = 0; i < sine_lut_size_1; i++) {
-                lut[position++] = array_bit1[i];
+                lut[bit_start_index + i] = array_bit1[i];
             }
         } else {
             for (int i = 0; i < sine_lut_size_0; i++) {
-                lut[position++] = array_bit0[i];
+                lut[bit_start_index + i] = array_bit0[i];
             }
         }
     }
@@ -111,9 +119,8 @@ void buildByteLUT(uint8_t data) {
 void sendByte(uint8_t data) {
     buildByteLUT(data);  // Update byte_lut contents
     
-    // DAC settings already point to byte_lut, just trigger
     SensEdu_DAC_Enable(DAC_CH1);
     while (!SensEdu_DAC_GetBurstCompleteFlag(DAC_CH1));
     SensEdu_DAC_ClearBurstCompleteFlag(DAC_CH1);
-    //SensEdu_DAC_Disable(DAC_CH1); not needed because burst mode
+    SensEdu_DAC_Disable(DAC_CH1);  // Clean shutdown
 }
